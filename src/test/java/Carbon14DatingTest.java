b680bb1c1a09fc788d2f1ce9ad3002352a961e0a
someblockhead <stoutchimera@gmail.com>
2023-07-18 14:23:26 -0500

    first commit

---
+++ src/test/java/Carbon14DatingTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Carbon14DatingTest {
+    //#1 Build a test for calculateAge();
+    @Test
+    public void testCalculateAge() {
+        Carbon14Dating example = new Carbon14Dating(50.0);
+        example.calculateAge();
+
+        assertEquals(5730, example.calculateAge());
+    }
+    /*
+        #2 Build a test for getRemainingAmount() in the
+        scenario the given remaining value is <= 1.
+    */
+
+   /*
+        #3 Build a test for getRemainingAmount() in the
+        scenario the given remaining value is >= 1.
+    */
+
+    // #4 Build a test for the toString() method.
+}
